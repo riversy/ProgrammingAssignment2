ba6c5ef23b9f494e924b9940dfa9456d2cca6e66
Igor Goltsov <riversy@gmail.com>
2015-03-21 01:43:13 +0300

    Fix # Tabs should be replaced within spaces

--- cachematrix.R
+++ cachematrix.R
@@ -1,30 +1,29 @@
 ##
-#	Programming Assignment 2: Lexical Scoping
-#	for course "R Programming" on Coursera
+#   Programming Assignment 2: Lexical Scoping
+#   for course "R Programming" on Coursera
 #   by Igor Goltsov <riversy@gmail.com>
 ##
 
 makeCacheMatrix <- function(x = matrix()) {
 ## This functione prepares 
-## Cache instance for some matrix "x"	
+## Cache instance for some matrix "x"   
 ## Methods:
-#      get() 			retrieves the source of the matrix
-#	   set(y) 			store source matrix and nulled calculation results in the Global Scope 
-#	   setsolve(solve) 	stores calculations in the global scope 
-#	   getsolve() 		retrieves previously calculated value from Global Scope
+#      get()            retrieves the source of the matrix
+#      set(y)           store source matrix and nulled calculation results in the Global Scope 
+#      setsolve(solve)  stores calculations in the global scope 
+#      getsolve()       retrieves previously calculated value from Global Scope
 
-	m <- NULL # Flush local variable "m" if there were some calculations before 
+    m <- NULL # Flush local variable "m" if there were some calculations before 
 
-	# This method store source matrix for future uses 
-	# and reset "m" variable in Global environment
+    # This method store source matrix for future uses 
+    # and reset "m" variable in Global environment
     set <- function(y) {
-            x <<- y 
-            m <<- NULL
+        x <<- y 
+        m <<- NULL
     }
 
     # This function will retrieve us matrix source
     get <- function() x
-
 
     setsolve <- function(solve) m <<- solve
 
@@ -44,29 +43,29 @@ cacheSolve <- function(x, ...) {
 ## This method check if calculations were apllied before 
 ## If matrix was inverted before, it just retireves 
 ## previously calculated result.
-## If this is a first call, it will inverse the matrix	
-## what was stored in the Cache Instance and cache result there	
+## If this is a first call, it will inverse the matrix  
+## what was stored in the Cache Instance and cache result there 
 
     # Trying to get value from cache instance
-	value <- x$getsolve()
+    value <- x$getsolve()
     
     # If value isn't null we may just return that
-	if (!is.null(value)){
+    if (!is.null(value)){
 
-		# Drop a message to console about what going on
-		print("Getting cached value.")
-		return(value)
-	}
-	
-	# Here we need to proceed calculations
-	value <- solve(x$get(), ...)
+        # Drop a message to console about what going on
+        print("Getting cached value.")
+        return(value)
+    }
+    
+    # Here we need to proceed calculations
+    value <- solve(x$get(), ...)
 
-	# Drop a message about what going on
-	print("Save solved value")
+    # Drop a message about what going on
+    print("Save solved value")
 
-	# and save calculated value in the Cache Instance 
+    # and save calculated value in the Cache Instance 
     x$setsolve(value)
 
     # Retrieves calculated value
-	value
+    value
 }
